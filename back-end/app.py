0767107d96b41b2e05c9f822a444e84456e2a4d1
von <e1144089@u.nus.edu>
2023-09-09 16:39:19 +0800

    update back-end code using Flask

---
+++ back-end/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, jsonify
+from mysql_operator import *
+from numpy import load
+import re
+
+app = Flask(__name__)
+
+# 选择题目，返回题目
+@app.route('/test', methods=['POST'])
+def start_testing():  # put application's code here
+    num_question = int(request.form["numQuestions"])
+    questions = randomly_select_data(num_question)
+    if len(questions) < num_question:
+        return jsonify({"code": -1, "message": "Don't have enough data", "data":{}})
+    return_dict = {"code": 0, "message": "Success", "data": []}
+    for i in range(len(questions)):
+        question_path = "npy_files/row_" + str(questions[i][0]) + ".npy"
+        data = load(question_path)
+        return_dict["data"].append({"id": questions[i][0],
+                                    "points": list(data),
+                                    "correct_answer": questions[i][1]})
+    return jsonify(return_dict)
+
+
+# 上传错题
+@app.route('/upload_mistakes', methods=['POST'])
+def upload_mistakes():
+    questions = re.findall("(-?[0-9]\d*)", request.form["mistakes"])
+    uid = request.form["uid"]
+    status_code, message = insert_mistakes(uid, questions)
+    return jsonify({"code": status_code, "message": message})
+
+
+# 上传favorite
+@app.route('/upload_favorites', methods=['POST'])
+def upload_favorites():
+    questions = re.findall("(-?[0-9]\d*)", request.form["favorites"])
+    uid = request.form["uid"]
+    status_code, message = insert_favorite(uid, questions)
+    return jsonify({"code": status_code, "message": message})
+
+
+# 返回所有错题
+@app.route('/get_mistakes', methods=['POST'])
+def get_mistakes():
+    uid = request.form["uid"]
+    questions = select_mistakes(uid)
+    return_dict = {"code": 0, "message": "Success", "data": []}
+    for i in range(len(questions)):
+        question_path = "npy_files/row_" + str(questions[i][0]) + ".npy"
+        data = load(question_path)
+        return_dict["data"].append({"id": questions[i][0],
+                                    "points": list(data),
+                                    "correct_answer": questions[i][1]})
+    return jsonify(return_dict)
+
+# 返回所有favorite
+@app.route('/get_favorites', methods=['POST'])
+def get_favorites():
+    uid = request.form["uid"]
+    questions = select_favorites(uid)
+    return_dict = {"code": 0, "message": "Success", "data": []}
+    for i in range(len(questions)):
+        question_path = "npy_files/row_" + str(questions[i][0]) + ".npy"
+        data = load(question_path)
+        return_dict["data"].append({"id": questions[i][0],
+                                    "points": list(data),
+                                    "correct_answer": questions[i][1]})
+    return jsonify(return_dict)
+
+if __name__ == '__main__':
+    app.run()
---
+++ back-end/mysql_operator.py
@@ -0,0 +1,101 @@
+import pymysql
+
+host = "127.0.0.1"
+port = 3306
+db_name = "HUAWEI"
+user_name = "root"
+password = "QWSqws0710@"
+
+def test():
+    conn = create_connection()
+    with conn.cursor() as cursor:
+        sql = """
+                SELECT VERSION();
+              """
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        print(data)
+    conn.close()
+
+def create_connection():
+    conn = pymysql.connect(host=host,
+                           port=port,
+                           db=db_name,
+                           user=user_name,
+                           password=password
+                           )
+    return conn
+
+
+def randomly_select_data(num_question):
+    conn = create_connection()
+    with conn.cursor() as cursor:
+        sql = "SELECT question_id, correct_answer FROM Questions ORDER BY RAND() LIMIT %s;"
+        cursor.execute(sql, num_question)
+        data = cursor.fetchall()
+    conn.close()
+    return data
+
+
+def insert_mistakes(user_id, question_ids):
+    conn = create_connection()
+    for question_id in question_ids:
+        try:
+            with conn.cursor() as cursor:
+                sql = "INSERT INTO Mistakes (question_id, user_id) VALUES (%s, %s)"
+                cursor.execute(sql, [str(question_id), str(user_id)])
+                conn.commit()
+        except pymysql.err.OperationalError:
+            conn.close()
+            return -1, "Failed in inserting mistakes"
+    conn.close()
+    return 0, "Succeed in inserting mistakes"
+
+
+def insert_favorite(user_id, question_ids):
+    conn = create_connection()
+    for question_id in question_ids:
+        try:
+            with conn.cursor() as cursor:
+                sql = "INSERT INTO Favorites (question_id, user_id) VALUES (%s, %s)"
+                cursor.execute(sql, [str(question_id), str(user_id)])
+                conn.commit()
+        except pymysql.err.OperationalError:
+            conn.close()
+            return -1, "Failed in inserting favorites"
+    conn.close()
+    return 0, "Succeed in inserting favorites"
+
+def select_mistakes(user_id):
+    conn = create_connection()
+    with conn.cursor() as cursor:
+        sql =   """
+                SELECT
+                    question_id, correct_answer
+                FROM
+                    Questions
+                WHERE
+                    question_id in (
+                        SELECT DISTINCT question_id FROM Mistakes WHERE user_id = %s)
+                """
+        cursor.execute(sql, [str(user_id)])
+        questions = cursor.fetchall()
+    conn.close()
+    return questions
+
+def select_favorites(user_id):
+    conn = create_connection()
+    with conn.cursor() as cursor:
+        sql = """
+                SELECT
+                    question_id, correct_answer
+                FROM
+                    Questions
+                WHERE
+                    question_id in (
+                        SELECT DISTINCT question_id FROM Favorites WHERE user_id = %s)
+                """
+        cursor.execute(sql, [str(user_id)])
+        data = cursor.fetchall()
+    conn.close()
+    return data
